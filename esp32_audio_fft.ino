7481ae855159edb7c997ea423bd88535adb489c5
Isha <139158006+ishXD@users.noreply.github.com>
2024-05-08 16:54:41 +0530

    Added files via upload

---
+++ esp32_audio_fft.ino
@@ -0,0 +1,90 @@
+#include <Wire.h>
+#include <Arduino.h>
+#include <U8g2lib.h>
+#include <arduinoFFT.h>
+
+// comment this to save some memory:
+#define USE_32BIT_SAMPLING
+
+// attach a LED to GPIO 17
+#define LED_PIN GPIO_NUM_17
+#define DEBUG_PIN GPIO_NUM_16
+
+#define SAMPLE_BUFFER_SIZE 512  // Number of samples
+
+//Create FFT object
+ArduinoFFT<double> FFT = ArduinoFFT<double>();
+//Computational domain of FFT:
+double vReal[SAMPLE_BUFFER_SIZE];
+double vImag[SAMPLE_BUFFER_SIZE];
+
+
+// raw waveform data:
+#ifdef USE_32BIT_SAMPLING
+#define MIC_BITS_PER_SAMPLE  I2S_BITS_PER_SAMPLE_32BIT
+int32_t wave[SAMPLE_BUFFER_SIZE];
+#else
+#define MIC_BITS_PER_SAMPLE  I2S_BITS_PER_SAMPLE_16BIT
+int16_t wave[SAMPLE_BUFFER_SIZE];
+#endif
+double maxWave = 0.0;  // to compute scaling factor for wave display
+
+void setup()
+{
+  pinMode(DEBUG_PIN, OUTPUT);   // for execution time measurement
+  pinMode(LED_PIN, OUTPUT);     // LED
+
+  // Wire.setClock(400000);     // this has no effect
+  Serial.begin(115200);
+
+  initMicrophone();
+
+  initDisplay();
+
+  delay(1000);
+}
+
+void loop()
+{
+  // Read waveform (5ms)
+  digitalWrite(DEBUG_PIN, HIGH);
+  digitalWrite(LED_PIN, HIGH);         // LED lights during sampling
+
+  if (getWave() != ESP_OK)
+  {
+    Serial.println("Error: i2s_read() failed");
+    displayError("i2s_read() failed");
+    delay(100);
+    return;
+  }
+
+  digitalWrite(LED_PIN, LOW);
+  digitalWrite(DEBUG_PIN, LOW);
+ 
+  performFFT();
+ 
+  displayAll(); 
+} 
+
+void performFFT()
+  {
+    for(int i=0;i<SAMPLE_BUFFER_SIZE;i++){
+      vReal[i] = wave[i] * 3.3 / 4096.0; //convert to voltage
+      vImag[i] = 0;
+    }
+    //Calculate FFT
+    FFT.windowing(vReal, SAMPLE_BUFFER_SIZE, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+
+    FFT.compute(vReal, vImag, SAMPLE_BUFFER_SIZE, FFT_FORWARD);
+
+    FFT.complexToMagnitude(vReal, vImag, SAMPLE_BUFFER_SIZE); //Calculate absolute value
+    
+  }
+
+
+
+
+
+
+
+// (Loop execution time: 82ms)
