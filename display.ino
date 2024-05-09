dea8b125ab227ea49005f188d96504ab5d3498f1
Isha <139158006+ishXD@users.noreply.github.com>
2024-05-09 11:55:53 +0530

    Update display.ino

--- display.ino
+++ display.ino
@@ -13,28 +13,27 @@ void initDisplay()
 
 void displayError(const char* msg)
 {
-  u8g2.clearBuffer();   // Screen buffer clear (22us)
-  u8g2.setFont(u8g2_font_crox1cb_tf); // With a larger font
+  u8g2.clearBuffer();   // Screen buffer clear 
+  u8g2.setFont(u8g2_font_crox1cb_tf); 
   u8g2.drawStr( 0, 0, "Error:");
   u8g2.setFont(u8g2_font_6x10_tf);
   u8g2.drawStr( 0, 20, msg);
-  u8g2.sendBuffer();    // (30ms)
+  u8g2.sendBuffer();    
   delay(1);             // Magic against write failure
 }
 
 void displayAll()
 {
-  u8g2.clearBuffer();   // Screen buffer clear (22us)
-  showWaveform();       // Waveform display (6ms)
-  showSpectrum();       // Spectrum display (9.4ms)
-  showOthers();         // Show other gridlines (1.6ms)
-  u8g2.sendBuffer();    // (30ms)
+  u8g2.clearBuffer();   
+  showWaveform();       
+  showOthers();         // Show other gridlines
+  u8g2.sendBuffer();    
   delay(1);             // Magic against write failure
 }
 
 // screen coordinates:
 #define PX2 0  // screen origin (left side edge)
-#define PY1 16 // Bottom edge of waveform screen (counting from the top edge) - also yellow zone
+#define PY1 16 // Bottom edge of waveform screen
 #define PY2 55 // lower edge of spectrum screen (-50db)
 
 void showWaveform() {   // Input waveform display
@@ -44,14 +43,13 @@ void showWaveform() {   // Input waveform display
     int x = PX2 + i;
     int y1 = PY1 - wave[i] / factor - offset;
     int y2 = PY1 - wave[i + 1] / factor - offset;
-    //u8g2.drawLine(PX2 + i, PY1 - (wave[i * 2]) / 256, PX2 + i + 1, 16 - (wave[i * 2 + 1] / 256)); // waveform plot
     u8g2.drawLine(x, y1, x + 1, y2); // waveform plot
   }
 }
  
 void showSpectrum() {     // Spectrum display
   int d;
-  static int peak[128];    // past peak value
+  static int peak[128];    // prev peak value
  
   for (int xi = 1; xi < 128; xi++) {       // Display spectrum (skip 0)
     d = barLength(vReal[xi]);
@@ -66,9 +64,9 @@ void showSpectrum() {     // Spectrum display
   }
 }
 
-//
+
 // Calculate length of spectrum graph
-//
+
 int barLength(double d)
 {
   float fy;
@@ -79,12 +77,11 @@ int barLength(double d)
   return y;
 }
 
-//
-// Graph decoration (drawing scales, etc.)
-//
+
+// Graph decoration
+
 void showOthers()
 {
-  //u8g2.drawVLine(PX2, 0, 64);         // screen origin line (vertical axis)
   u8g2.drawHLine(0, PY2, 128);          // Spectrum bottom line
  
   // frequency scale (horizontal axis)
@@ -94,7 +91,7 @@ void showOthers()
   u8g2.drawVLine(PX2 + 25, PY2 + 1, 2);   // 5k ticks
   u8g2.drawVLine(PX2 + 50, PY2 + 1, 2);   // 10k ticks
  
-  u8g2.setFont(u8g2_font_micro_tr);       // Small font (3x5),
+  u8g2.setFont(u8g2_font_micro_tr);       
   u8g2.setCursor(0, 58); u8g2.print("0"); // R side frequency display
   u8g2.setCursor(62, 58); u8g2.print("1k");
   u8g2.setCursor(120, 58); u8g2.print("2k");
@@ -105,12 +102,10 @@ void showOthers()
       u8g2.drawHLine(x, y, 2);
     }
   }
-  u8g2.setFont(u8g2_font_6x10_tf); // larger font
-  u8g2.setCursor(110, 17); u8g2.print("0dB"); // spectral sensitivity
-  u8g2.setCursor(110, 30); u8g2.print("-20"); //
-  u8g2.setCursor(110, 45); u8g2.print("-40"); //
+  u8g2.setFont(u8g2_font_6x10_tf); 
+  u8g2.setCursor(110, 17); u8g2.print("0dB"); 
+  u8g2.setCursor(110, 30); u8g2.print("-20"); 
+  u8g2.setCursor(110, 45); u8g2.print("-40"); 
  
-  // LR display
-  //u8g2.setFont(u8g2_font_crox1cb_tf); // With a slightly gorgeous font
-  //u8g2.setCursor(0, 3); u8g2.print("R"); //
+ 
 }
