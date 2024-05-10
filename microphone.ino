9a46ed7469166facddb6adfca28cd67ff3f55127
Isha <139158006+ishXD@users.noreply.github.com>
2024-05-10 22:23:38 +0530

    Update microphone.ino

--- microphone.ino
+++ microphone.ino
@@ -1,20 +1,15 @@
 #include <driver/i2s.h>
 
-// connect your INMP441 breakout as following, with L/R - to VCC 3.3V:
+// connect your INMP441 pins as following, with L/R - to VCC 3.3V:
 #define I2S_WS GPIO_NUM_15
 #define I2S_SD GPIO_NUM_13
 #define I2S_SCK GPIO_NUM_2
 #define I2S_PORT I2S_NUM_0
 
-//#define SAMPLE_RATE 44100
 #define SAMPLE_RATE 8000
 
-// most microphones will probably default to left channel but you may need to tie the L/R pin high:
+// for microphone left channel set the L/R pin high:
 #define I2S_MIC_CHANNEL I2S_CHANNEL_FMT_ONLY_LEFT
-
-// also see .bits_per_sample below:
-//int16_t wave[SAMPLE_BUFFER_SIZE];
-//int32_t wave[SAMPLE_BUFFER_SIZE];
 
 void initMicrophone() 
 {
@@ -58,7 +53,7 @@ void i2s_install() {
     .bits_per_sample = MIC_BITS_PER_SAMPLE, // I2S_BITS_PER_SAMPLE_16BIT or I2S_BITS_PER_SAMPLE_32BIT
     .channel_format = I2S_MIC_CHANNEL,      // L/R to high - left, L/R to ground - right channel
     .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
-    .intr_alloc_flags = 0, // default interrupt priority
+    .intr_alloc_flags = 0, // default 
     //.intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
     .dma_buf_count = 4,
     .dma_buf_len = 1024,
@@ -80,18 +75,3 @@ void i2s_setpin() {
 
   i2s_set_pin(I2S_PORT, &pin_config);
 }
-
-/*
-// Analog version:
-
-// Analog audio input pin, ADC2_CH0 (GPIO 4) 
-#define MIC_ADC_PIN GPIO_NUM_4
-
-bool getWave()
-{
-  for (int i = 0; i < samples; i++) {
-    wave[i] = analogRead(MIC_ADC_PIN);  // Get waveform data (fs:4096)
-    delayMicroseconds(42);              // Sampling period adjustment (1 cycle 39us)
-  }                                     // (5ms)
-}
-*/
